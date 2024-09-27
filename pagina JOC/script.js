2ac78cf164d1900555383d61edd062440aa89c9b
julospina <julianospina@outlook.com>
2024-09-27 12:52:23 -0300

    Delete pagina JOC directory

--- pagina JOC/script.js
+++
@@ -1,9 +0,0 @@
-// Mostrar/Ocultar links al hacer clic
-document.getElementById('toggle-links').addEventListener('click', function() {
-    var linkContainer = document.getElementById('linkContainer');
-    if (linkContainer.style.display === 'none') {
-        linkContainer.style.display = 'block';
-    } else {
-        linkContainer.style.display = 'none';
-    }
-});
